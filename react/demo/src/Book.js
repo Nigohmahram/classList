5a5661bacc490c7b363229825a9fae2229a5658a
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-07-15 01:25:51 +0900

    React ternery operator

--- react/demo/src/Book.js
+++ react/demo/src/Book.js
@@ -1,4 +1,19 @@
 export default function Book(props) {
+      // if(!props.name){
+      //       return null
+      // }else{
+      //       document.body.textContent = 'salom'
+      // }
+      // return !props.name ?(
+      //       <div>
+      //       <h2>{(props.name) ? <span>{props.name}</span> : 'default book'}</h2>
+      //       <p>{props.year}</p>
+      //       <p>{props.price}</p>
+      //       <p>{props.children}</p>
+      //       </div>
+      // ) : (
+      //       <h1>Ma'lumot topilmadi</h1>
+      // )
       return(
             <div>
             <h2>{(props.name) ? <span>{props.name}</span> : 'default book'}</h2>
